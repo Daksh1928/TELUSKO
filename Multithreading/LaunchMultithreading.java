4e7b6c0610f70fe9e7ad806e56102ffc7974ade5
Devanshu Rana <devanshurana84@gmail.com>
2026-03-13 21:07:51 +0530

    MultiThreading first part completed

---
+++ Multithreading/LaunchMultithreading.java
@@ -0,0 +1,10 @@
+package Multithreading;
+
+public class LaunchMultithreading {
+    public static void main(String[] args) throws InterruptedException {
+        IO.print("args");
+        Thread.sleep(6000);
+        IO.print("Thread end ");
+
+    }
+}
